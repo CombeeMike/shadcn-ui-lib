ffc0ce92c8942d20b7b9bf7bf20cc3885abdae61
HAR <michael.harrer@combeenation.com>
2024-02-17 10:54:23 +0100

    Simplify code for Stackoverflow question

--- CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-# Version 0.0.1
-
-xxx, yy.zz.2024
-
-- Initial release with components:
-  `Avatar`, `Button`, `Card`, `DropdownMenu`, `Heading`, `LoadingSpinner`, `Markdown`, `Separator`, `Form`, `Label`, `Input`, `SimpleFormField` & `Tooltip`
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,7 @@
 export * from './lib/css.utils.js';
 
-export * from './components/button.js';
 export * from './components/card.js';
 export * from './components/heading.js';
-export * from './components/loading-spinner.js';
-export * from './components/avatar.js';
-export * from './components/dropdown-menu.js';
-export * from './components/tooltip.js';
-export * from './components/separator.js';
-export * from './components/markdown.js';
 export * from './components/label.js';
 export * from './components/form.js';
-export * from './components/simple-form-field.js';
 export * from './components/input.js';
